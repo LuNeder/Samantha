5c5dc23249215c3fc7415793b91ae0f6783d2a1f
LuNeder <luananeder@gmail.com>
2021-03-30 13:29:49 -0300

    doing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 // Copyright (c) 2021 Lu Neder
 // Licensed under the Open Software License version 3.0
+
+// import needed crates
+use toml::value; //required for config.toml, account.toml and other .toml files
+use std::env; //required for grabbing command line args
+
+
+
 fn main() {
+    let ver: String = "1.0.0".to_string(); //sets ver vaiable to current Samantha version
+
+    // Get command args and save it as an args variable
+    let args: Vec<String> = env::args().collect();
+    let option = &args[1];
+    if option = "help" {
+        sam_help(ver)
+    }
     println!("WORK IN PROGRESS");
+}
+
+fn sam_help(ver: String) {
+    println!("Samantha {}", ver);
+    
 }
