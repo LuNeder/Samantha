c0f92c6b4bfc94f15d43607f3d7ff013d9303bdf
LuNeder <luananeder@gmail.com>
2021-04-01 15:27:29 -0300

    Update main.rs
    Commenting

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ use config::*; //required for config.toml, account.toml and other .toml files
 use std::env; //required for grabbing command line args
 use std::fs; //required to move and rename fles (fs::rename), to copy files (fs::copy) and to create directories (fs::create_dir_all)
 use symlink::*; //required to create symbolic links
-use std::path::Path;
-use std::collections::HashMap;
-use glob::glob;
-use std::io;
+use std::path::Path; // required for file paths
+use std::collections::HashMap; // required for .toml files
+use glob::glob; // required to get .toml files
+use std::io; // a bunch of stuff
 
 
 fn main() {
@@ -19,6 +19,7 @@ fn main() {
 
     // Get command args and save it as an args variable
     let args: Vec<String> = env::args().collect();
+    // run the option the user chose (--help, --init, etc)
     let option = &args[1];
     if option == "help" {
         sam_help(ver);
@@ -29,7 +30,7 @@ fn main() {
 }
 
 
-//Help
+// Print Help
 fn sam_help(ver: String) {
     println!("Samantha {}", ver);
     println!("IMPORTANT - All file paths must be absolute. Do not use relative paths (like ./ or ../), they will not work.");
@@ -43,8 +44,9 @@ fn sam_help(ver: String) {
 
 // First usage stuff
 fn init() {
-    let args: Vec<String> = env::args().collect();
-    let configpath = &args[2];
+    let args: Vec<String> = env::args().collect(); //Get command line args
+    let configpath = &args[2]; // Get the 2nd arg (Config.toml path)
+    //Get the stuff in Config.toml
     let mut settings = Config::default();
     settings
         .merge(glob(configpath)
@@ -52,18 +54,19 @@ fn init() {
             .map(|path| File::from(path.unwrap()))
             .collect::<Vec<_>>())
         .unwrap();
-
+// Save the Config.toml as a config variable as a HashMap
     let mut config =
         settings
         .try_into::<HashMap<String, String>>()
         .unwrap();
-    println!("{:#?}", config);
-    let samantha_root = &config["samantha_root"];
-    let newconfigpath = samantha_root.to_owned() + "/Config.toml";
-    rootdir(samantha_root.to_string()).expect("error creating Samantha root");
-    moveconfig(configpath.to_string(), newconfigpath.to_string()).expect("error copying Config.toml");
+    println!("{:#?}", config); //prints the config
+    let samantha_root = &config["samantha_root"]; //get the samantha_root configuration and saves it as a samantha_root variable
+    let newconfigpath = samantha_root.to_owned() + "/Config.toml"; // path where Config.toml will be copied to
+    rootdir(samantha_root.to_string()).expect("error creating Samantha root"); //runs the Function that creates the Samantha root directory
+    moveconfig(configpath.to_string(), newconfigpath.to_string()).expect("error copying Config.toml");//Runs the Function that copies Config.toml to Samantha root
 
 }
+//Function that creates the Samantha root directory
 fn rootdir(samantha_root: String) -> std::io::Result<()> {
     println!("Samantha root: {}", samantha_root);
     fs::create_dir_all(samantha_root)?;
