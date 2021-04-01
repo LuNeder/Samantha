daea07b1ec118d5fe3702e799a4ae5b453ae2079
LuNeder <luananeder@gmail.com>
2021-04-01 14:44:49 -0300

    doing
    Currently trying to discover why tf it doesn't create the directory and doesn't copy the file

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@
 // Licensed under the Open Software License version 3.0
 
 // import needed crates
-use toml::value; //required for config.toml, account.toml and other .toml files
+extern crate config;
+extern crate glob;
+use config::*; //required for config.toml, account.toml and other .toml files
 use std::env; //required for grabbing command line args
-use std::fs::{rename, copy}; //required to move and rename fles (fs::rename) and to copy files (fs::copy)
+use std::fs; //required to move and rename fles (fs::rename), to copy files (fs::copy) and to create directories (fs::create_dir_all)
 use symlink::*; //required to create symbolic links
+use std::path::Path;
+use std::collections::HashMap;
+use glob::glob;
+use std::io;
 
 
 fn main() {
@@ -14,8 +20,10 @@ fn main() {
     // Get command args and save it as an args variable
     let args: Vec<String> = env::args().collect();
     let option = &args[1];
-    if option == "--help" {
-        sam_help(ver)
+    if option == "help" {
+        sam_help(ver);
+    } else if option == "init" {
+        init();
     }
     println!("WORK IN PROGRESS");
 }
@@ -28,13 +36,43 @@ fn sam_help(ver: String) {
     println!("IMPORTANT - While not required, we recommend you to NOT add spaces to the filenames or to the directory names");
     println!("--help: Show this help list");
     println!("--init [/PATH/TO/Config.toml]: Configure Samantha for the first time, following the Config.toml file with the options you chose.");
-
+    println!("--create-account")
 }
 
 
 // First usage stuff
 fn init() {
+    let args: Vec<String> = env::args().collect();
+    let configpath = &args[2];
+    let mut settings = Config::default();
+    settings
+        .merge(glob(configpath)
+            .unwrap()
+            .map(|path| File::from(path.unwrap()))
+            .collect::<Vec<_>>())
+        .unwrap();
 
+    let mut config =
+        settings
+        .try_into::<HashMap<String, String>>()
+        .unwrap();
+    println!("{:#?}", config);
+    let samantha_root = &config["samantha_root"];
+    let newconfigpath = samantha_root.to_owned() + "/Config.toml";
+    rootdir(samantha_root.to_string());
+    moveconfig(configpath.to_string(), newconfigpath.to_string());
+
+}
+
+fn rootdir(samantha_root: String) -> std::io::Result<()> {
+    println!("Samantha root: {}", samantha_root);
+    fs::create_dir_all(samantha_root)?;
+    Ok(())
+}
+fn moveconfig(configpath: String, newconfigpath: String) -> std::io::Result<()> {
+    println!("Config: {}", newconfigpath);
+    fs::copy(configpath, newconfigpath)?;
+    Ok(())
 }
 
 
